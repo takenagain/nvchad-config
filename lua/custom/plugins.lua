f2de47ad6ce18ba97d0128b472c0a1b0de43f5ce
Francois <=>
2023-09-14 22:11:22 +0200

    rust debugging and debugging shortcuts

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -30,7 +30,7 @@ local plugins = {
       "mfussenegger/nvim-dap",
       "rcarriga/nvim-dap-ui",
     },
-    config = function(_, opts)
+    config = function()
       local virtual = os.getenv "VIRTUAL_ENV" or os.getenv "CONDA_DEFAULT_ENV" or "/usr"
       local path = virtual .. "/bin/python"
       require("dap-python").setup(path)
@@ -75,15 +75,37 @@ local plugins = {
   {
     "nvim-neotest/neotest-go",
   },
-  -- {
-  --   "simrat39/rust-tools.nvim",
-  --   lazy = false,
-  --   dependencies = {
-  --     "neovim/nvim-lspconfig",
-  --     "nvim-lua/plenary.nvim",
-  --     "mfussenegger/nvim-dap",
-  --   },
-  -- },
+  {
+    "simrat39/rust-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "nvim-lua/plenary.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    config = function()
+      local mason_registry = require "mason-registry"
+      local codelldb = mason_registry.get_package "codelldb"
+      local extension_path = codelldb:get_install_path() .. "/extension/"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+      require("rust-tools").setup {
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        dap = {
+          adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+        server = {
+          on_attach = function(_, bufnr)
+            local rt = require "rust-tools"
+            -- Hover actions
+            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+          end,
+        },
+      }
+    end,
+  },
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",

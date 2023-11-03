dbd6020fe318da53fd0773d086c3358f8e61d49a
Francois <francoisvw@protonmail.com>
2023-11-03 08:47:04 +0200

    Friendship with flutter-tools ended

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -331,44 +331,44 @@ local plugins = {
   },
 
   -- Dart
-  {
-    "akinsho/flutter-tools.nvim",
-    lazy = false,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "stevearc/dressing.nvim", -- optional for vim.ui.select
-      "mfussenegger/nvim-dap",
-    },
-    config = function()
-      require("flutter-tools").setup {
-        debugger = { -- integrate with nvim dap + install dart code debugger
-          enabled = true,
-          run_via_dap = true, -- use dap instead of a plenary job to run flutter apps
-          register_configurations = function(_)
-            require("dap").configurations.dart = {}
-            require("dap.ext.vscode").load_launchjs()
-          end,
-        },
-        dev_tools = {
-          autostart = true, -- autostart devtools server if not detected
-          auto_open_browser = true, -- Automatically opens devtools in the browser
-        },
-        decorations = {
-          statusline = {
-            -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
-            -- this will show the current version of the flutter app from the pubspec.yaml file
-            app_version = true,
-            -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
-            -- this will show the currently running device if an application was started with a specific
-            -- device
-            device = true,
-            -- set to true to be able use the 'flutter_tools_decorations.project_config' in your statusline
-            -- this will show the currently selected project configuration
-            project_config = true,
-          },
-        },
-      }
-    end,
-  },
+  -- {
+  --   "akinsho/flutter-tools.nvim",
+  --   lazy = false,
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "stevearc/dressing.nvim", -- optional for vim.ui.select
+  --     "mfussenegger/nvim-dap",
+  --   },
+  --   config = function()
+  --     require("flutter-tools").setup {
+  --       debugger = { -- integrate with nvim dap + install dart code debugger
+  --         enabled = true,
+  --         run_via_dap = true, -- use dap instead of a plenary job to run flutter apps
+  --         register_configurations = function(_)
+  --           require("dap").configurations.dart = {}
+  --           require("dap.ext.vscode").load_launchjs()
+  --         end,
+  --       },
+  --       dev_tools = {
+  --         autostart = true, -- autostart devtools server if not detected
+  --         auto_open_browser = true, -- Automatically opens devtools in the browser
+  --       },
+  --       decorations = {
+  --         statusline = {
+  --           -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
+  --           -- this will show the current version of the flutter app from the pubspec.yaml file
+  --           app_version = true,
+  --           -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
+  --           -- this will show the currently running device if an application was started with a specific
+  --           -- device
+  --           device = true,
+  --           -- set to true to be able use the 'flutter_tools_decorations.project_config' in your statusline
+  --           -- this will show the currently selected project configuration
+  --           project_config = true,
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
 }
 return plugins
